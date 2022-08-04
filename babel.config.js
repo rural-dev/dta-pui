4c23ff5ebfd38cd800e42c5f521dfd8530d97dc6
Rural Developer <ruraldevusa@gmail.com>
2022-08-04 14:23:47 +0700

    update

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,27 @@
+const {NODE_ENV} = process.env;
+const inDevelopment = NODE_ENV === 'development';
+
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-       [
-        "tailwindcss-react-native/babel",
-        { blockModuleTransform: ["react-select"] }
-       ]
-     ],
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+        [
+            'tailwindcss-react-native/babel',
+            {blockModuleTransform: ['react-select']},
+        ],
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                exclude: !inDevelopment
+                    ? /node_modules\/(?!(react-native-ratings))/
+                    : /(node_modules)/,
+                options: {
+                    cacheDirectory: inDevelopment,
+                    cacheCompression: false,
+                },
+            },
+        ],
+    },
 };
